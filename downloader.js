e46d43b039c6756d54df90acc89ebaa31f61cb27
X246072 <eduardo.saavedra@wnco.com>
2020-01-07 16:17:41 -0500

    better download with mapLimit

--- downloader.js
+++ downloader.js
@@ -18,15 +18,26 @@ async function download(url, path) {
     });
 }
 
-fs.readFile("./The Complete Node.js Course.json", 'utf8', function(err, data) {
+fs.readFile("./Mastering React.json", 'utf8', function(err, data) {
     if (err) throw err;
     urls = JSON.parse(data);
     let idx = 0;
-    async.each(urls, function(i) {
+/*     async.each(urls, function(i) {
         let k = Object.keys(urls)[idx];
         idx++;
         download(i, Path.resolve(__dirname, './vids/', k + '.mp4'))
-            .then(data => console.info('finished: ', i))
+            .then(data => console.info('finished: ', k))
             .catch(err => console.info(err));
+    }); */
+    async.mapLimit(urls, 5, async function(i) {
+        let k = Object.keys(urls)[idx];
+        idx++;
+        return download(i, Path.resolve(__dirname, './vids/', k + '.mp4'))
+            .then(data => console.info('finished: ', k))
+            .catch(err => console.info(err));
+    }, (err, results) => {
+        if (err) throw err
+        // results is now an array of the response bodies
+        console.log(results)
     })
 });
