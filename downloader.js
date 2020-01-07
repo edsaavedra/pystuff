446f71298cce1ebaca638f8a3a9ae10c8cfd3a89
X246072 <eduardo.saavedra@wnco.com>
2020-01-07 15:36:18 -0500

    add async library

--- downloader.js
+++ downloader.js
@@ -1,28 +1,32 @@
+const async = require('async')
+const Axios = require("axios");
 const fs = require("fs");
 const Path = require("path");
-const Axios = require("axios");
 let urls;
 
-fs.readFile("./Python Programming for Developers.json", 'utf8', function(err, data) {
+async function download(url, path) {
+    const response = await Axios({
+        method: 'GET',
+        url: url,
+        responseType: 'stream'
+    });
+
+    response.data.pipe(fs.createWriteStream(path));
+    return new Promise((resolve, reject) => {
+        response.data.on('end', () => {resolve()});
+        response.data.on('error', err => {reject(err)})
+    });
+}
+
+fs.readFile("./The Complete Node.js Course.json", 'utf8', function(err, data) {
     if (err) throw err;
     urls = JSON.parse(data);
-    Object.keys(urls).forEach(function(i) {
-        download(urls[i], Path.resolve(__dirname, './vids/', i + '.mp4'))
+    let idx = 0;
+    async.each(urls, function(i) {
+        let k = Object.keys(urls)[idx];
+        idx++;
+        download(i, Path.resolve(__dirname, './vids/', k + '.mp4'))
             .then(data => console.info('finished: ', i))
-            .catch(err => console.info(err))
+            .catch(err => console.info(err));
     })
 });
-
-async function download(url, path) {
-        const response = await Axios({
-            method: 'GET',
-            url: url,
-            responseType: 'stream'
-        });
-
-        response.data.pipe(fs.createWriteStream(path));
-        return new Promise((resolve, reject) => {
-            response.data.on('end', () => {resolve()});
-            response.data.on('error', err => {reject(err)})
-        });
-}
